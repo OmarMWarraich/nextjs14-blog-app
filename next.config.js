54f183a4e6e719216357608be826cf1835b8e086
OmarMWarraich <earthling986@gmail.com>
2025-01-02 23:11:43 +0500

    Init

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['avatars.githubusercontent.com', 'images.pexels.com', 'www.wordstream.com', 'i.ytimg.com']
+    }
+}
+
+module.exports = nextConfig
